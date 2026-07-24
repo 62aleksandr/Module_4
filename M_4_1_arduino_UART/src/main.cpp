243a86ccc611bd96e46883b90896a956c19a229f
Oleksandr <62a.aleksandr@gmail.com>
2026-07-24 17:28:53 +0200

    M_4_1_24_07_26

---
+++ M_4_1_arduino_UART/src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+
+#define LED_PIN 15
+#define UART1_TX_PIN 17
+#define UART1_RX_PIN 18
+
+// Використовуємо UART1 на пінах GPIO17 (TX) та GPIO18 (RX)
+HardwareSerial uart1(1);
+
+void setup()
+{
+  Serial.begin(115200); // Монітор через USB (UART0)
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  uart1.begin(9600, SERIAL_8N1, UART1_RX_PIN, UART1_TX_PIN); // UART1: 9600 бод, 8N1, RX=18, TX=17
+
+  Serial.println("ESP32-S3 UART is ready to communicate!");
+}
+
+void loop()
+{
+  if (uart1.available())
+  {
+    byte ch = uart1.read();
+    Serial.print("Received UART1: ");
+    Serial.println(ch, HEX); // Виводимо у HEX для діагностики
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  }
+
+  if (Serial.available())
+  {
+    byte cmd = Serial.read();
+    Serial.print("Received from Serial Monitor: ");
+    Serial.println(cmd, HEX); // Виводимо команду з Serial Monitor у HEX
+    uart1.write(cmd);         // Передаємо команду з Serial Monitor на UART1
+  }
+}
